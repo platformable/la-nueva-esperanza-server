60f2678d0da6cb0f00ce89b728c2d99539119457
Alexei Garban <garban.valdeon@gmail.com>
2022-11-08 11:48:40 +0100

    new routes for support groups

--- controllers/supportGroupsControllers.js
+++ controllers/supportGroupsControllers.js
@@ -2,6 +2,15 @@ const db = require('../dbConnect')
 
 
 module.exports= {
+    getAllSupportGroups:async(req,res)=>{
+        try {
+            const allData = await db.query("select * from support_groups");
+            const response = allData.rows;
+            res.send(response);
+          } catch (e) {
+            res.send("an error ocurred");
+          }
+    },
    
     createNew: async (req,res)=> {
         console.log("support group req.body",req.body)
@@ -58,5 +67,77 @@ module.exports= {
             res.status(400).send({message:"FAIL"})
             console.log("error",e)
         }
-    }
+    },
+    updateSupportGroup: async (req, res) => {
+        let {
+            id,
+            supportMeetingDate ,
+            supportGroupName ,
+            supportGroupAudience ,
+            supportGroupTopic ,
+            supportGroupSummary,
+            supportGroupChallenges,
+            facilitator,
+            supportGroupStartTime,
+            supportGroupEndTime,
+            supportGroupSigned} = req.body;
+    
+        try {
+          const query = await {
+            name: "update-support-groups",
+            text: `update support_groups set 
+            id=$1,
+            supportMeetingDate=$2,
+            supportGroupName=$3 ,
+            supportGroupAudience=$4 ,
+            supportGroupTopic=$5 ,
+            supportGroupSummary=$6,
+            supportGroupChallenges=$7,
+            facilitator=$8,
+            supportGroupStartTime=$9,
+            supportGroupEndTime=$10,
+            supportGroupSigned=$11 where id=$1`,
+            values: [id,
+                supportMeetingDate ,
+                supportGroupName ,
+                supportGroupAudience ,
+                supportGroupTopic ,
+                supportGroupSummary,
+                supportGroupChallenges,
+                facilitator,
+                supportGroupStartTime,
+                supportGroupEndTime,
+                supportGroupSigned],
+          };
+          db
+            .query(query)
+            .then((response) =>
+              res.send({
+                data: response.rowCount,
+                status: 200,
+              })
+            )
+        } catch (error) {
+          res.json("an error ocurred");
+          console.log("error message:", error);
+        }
+      },
+      delete: async (req, res) => {
+        const { id } = req.body;
+        const query = {
+          text: "DELETE from support_groups where id=$1",
+          values: [id],
+        };
+        // promise
+        db.query(query)
+          .then((data) => {
+       
+              res.send({
+                status: "OK",
+                message: "support group deleted",
+              });
+         
+          })
+          .catch((e) => console.error(e.stack));
+      },
 }
--- routes/supportGroups.js
+++ routes/supportGroups.js
@@ -3,6 +3,10 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/supportGroupsControllers')
 
+router.get("/",controller.getAllSupportGroups)
 router.post("/",controller.createNew)
+router.put("/update",controller.updateSupportGroup)
+router.delete("/delete",controller.delete)
+
 
 module.exports = router
