74d47c9519bb7dcafc98a249912592d993e8d2e8
Alexei Garban <garban.valdeon@gmail.com>
2023-09-27 12:59:23 +0200

    services added

--- controllers/progressNotesControllers.js
+++ controllers/progressNotesControllers.js
@@ -9,24 +9,39 @@ module.exports= {
 
     const countQuery = 'SELECT * FROM progress_note';
     const countResult = await db.query(countQuery);
-    const totalRecords = countResult.rows.length;
-
-    
+    const totalRecords = await countResult.rows.length;
+    console.log("totalRecords",totalRecords)
 
    const page = parseInt(req.query.page)
    const limit = parseInt(req.query.limit)
    const offset= (page-1) * limit
 
     const totalPages = Math.ceil(totalRecords / limit);
-
-
-
+    console.log("totalPages",totalPages)
 
     const hasPreviousPage = page > 1;
     const hasNextPage = page < totalPages;
   
     
-  const query = {text:`SELECT id, clientfirstname, clientlastname, progressnotedate
+  const query = {text:`SELECT id, clientfirstname, clientlastname, progressnotedate,
+  developmentactionplan,
+  cd4vllabreport,
+transportationcoordination,
+translationinterpretation,
+comprehensivebehavioralriskassessment,
+ticklerupdate,
+treatmenteducation,
+preventioncounselling,
+supportivecounselling,
+escort,
+caseclosuredischarge,
+linkagetoservices,
+supportgroups,
+implementationactionplan,
+housingassistance,
+benefitsassistance,
+employmentassistance,
+otherassistance
   FROM progress_note
   order by id 
   LIMIT $1 OFFSET $2`,
@@ -47,8 +62,8 @@ module.exports= {
           res.send(newData);
     
   } catch (error) {
-    console.log(error)
-    res.send({errorMessage:error})
+    console.log("errorrrrr",error)
+    res.send({errorMessage:error, reviewError:'Check there is query params'})
   }
 
 
